3011f6747d153c4189b9edbf9e01d65be0af9661
Ivan Danilov <ida@inleadmedia.com>
2026-05-22 11:43:04 +0300

    EONEXT-252 - Fix styles for banner.

--- react/src/apps/material/material.stories.tsx
+++ react/src/apps/material/material.stories.tsx
@@ -901,7 +901,7 @@ const meta: Meta<typeof MaterialEntry> = {
     searchUrl: "/search",
     materialUrl: "/work/:workid",
     advancedSearchUrl: "/advancedsearch",
-    wid: "work-of:870970-basis:141339257",
+    wid: "work-of:870970-basis:141399357",
     agencyIdConfig: "710100",
     localSubjectsAgencyIdsConfig: "",
     smsNotificationsForReservationsEnabledConfig: "1",
