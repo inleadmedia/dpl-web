92fe17beddcb965b4b521ce9d79e8afbf5f05b6b
Dinu Rodnitchi <rodnitchi@gmail.com>
2026-06-25 13:18:52 +0300

    Revert "EONEXT-294: Disable go category from search."
    This reverts commit a9fdae5792d311a9ebf4302cab4a0cb99e1bc864.

--- cms/web/modules/custom/eonext_editorial_search/eonext_editorial_search.deploy.php
+++ cms/web/modules/custom/eonext_editorial_search/eonext_editorial_search.deploy.php
@@ -109,12 +109,3 @@ function eonext_editorial_search_deploy_index_past_events(): string {
 
   return eonext_editorial_search_ensure_event_has_upcoming_indexing();
 }
-
-/**
- * Excludes GO categories from editorial search index and facet filters.
- */
-function eonext_editorial_search_deploy_exclude_go_categories_from_index(): string {
-  _eonext_editorial_search_load_install();
-
-  return eonext_editorial_search_exclude_go_categories_from_index();
-}
