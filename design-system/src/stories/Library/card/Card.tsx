65b59d11583d3eedc9814b6f46f7dba68eb6bf86
Ivan Danilov <ida@inleadmedia.com>
2026-07-08 15:21:16 +0300

    EONEXT-319_react - Redesigned Swiper slider.

--- design-system/src/stories/Library/card/Card.tsx
+++ design-system/src/stories/Library/card/Card.tsx
@@ -1,24 +1,16 @@
 import { FC, ReactNode } from "react";
 import clsx from "clsx";
 import MediaContainer from "../media-container/MediaContainer";
+import { ReactComponent as ArrowSmallRight } from "../Arrows/icon-arrow-ui/icon-arrow-ui-small-right.svg";
 
 type CardProps = {
   variant?: string;
-  typeTag?: string;
-  dateTag?: string;
   title?: string;
   image?: ReactNode;
   placeholderText?: string;
 };
 
-const Card: FC<CardProps> = ({
-  variant,
-  image,
-  placeholderText,
-  title,
-  typeTag,
-  dateTag,
-}) => {
+const Card: FC<CardProps> = ({ variant, image, placeholderText, title }) => {
   const classes = clsx("card", {
     "card--has-no-media": !image,
     "card--has-media": !!image,
@@ -30,15 +22,17 @@ const Card: FC<CardProps> = ({
         <div className="card__media">
           <MediaContainer placeholderText={placeholderText} media={image} />
         </div>
-        <div className="card__tags">
-          {typeTag ? (
-            <span className="card__tag card__tag--type">{typeTag}</span>
+        <div className="card__text">
+          <h3 className="card__title">{title}</h3>
+          {placeholderText ? (
+            <p className="card__teaser">{placeholderText}</p>
           ) : (
             ""
           )}
-          {dateTag ? <span className="card__tag">{dateTag}</span> : ""}
+          <div className="card__arrow" aria-hidden="true">
+            <ArrowSmallRight />
+          </div>
         </div>
-        <h1 className="card__title">{title}</h1>
       </a>
     </article>
   );
