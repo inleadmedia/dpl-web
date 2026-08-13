13b0161cff4640807d58b54dacf332cdc7b753a4
Dinu Rodnitchi <rodnitchi@gmail.com>
2026-08-13 15:47:14 +0300

    EONEXT-323: Standalone modules build. Fix to string.

--- cms/web/modules/custom/eonext_react/js/hook-to-react.js
+++ cms/web/modules/custom/eonext_react/js/hook-to-react.js
@@ -30,25 +30,28 @@
         _package = Object.assign({}, _package);
 
       Object.keys(_package).forEach(minifiedKey => {
-        if (!_package[minifiedKey] || !_package[minifiedKey].toString)
-          return;
+        try {
+          const exportValue = _package[minifiedKey];
+          if (exportValue == null)
+            return;
 
-        let unwrapped;
-        if (typeof _package[minifiedKey] === "object" && unwrapPackageObject) {
-          unwrapped = unwrapPackageObject(_package[minifiedKey]);
-        } else {
-          try {
-            unwrapped = unwrapPackageMethod(_package[minifiedKey].toString());
-          } catch (_) {
+          let unwrapped;
+          if (typeof exportValue === "object" && unwrapPackageObject) {
+            unwrapped = unwrapPackageObject(exportValue);
+          } else if (typeof exportValue === "function") {
+            unwrapped = unwrapPackageMethod(exportValue.toString());
+          } else {
             return;
           }
-        }
 
-        if (unwrapped) {
-          _package[unwrapped.methodKey] = _package[minifiedKey];
-          _package[unwrapped.methodKey].minifiedKey = minifiedKey;
+          if (unwrapped) {
+            _package[unwrapped.methodKey] = exportValue;
+            _package[unwrapped.methodKey].minifiedKey = minifiedKey;
 
-          this._unwrappedPackages[unwrapped.packageKey] = _package;
+            this._unwrappedPackages[unwrapped.packageKey] = _package;
+          }
+        } catch (_) {
+          return;
         }
       });
     }
